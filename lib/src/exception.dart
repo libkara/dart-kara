5cc74550322083cd00440b447b62fd4edb87cac5
Brian Wo <45139213+brainwo@users.noreply.github.com>
2024-08-09 23:23:36 +0800

    Add missing license header
    Signed-off-by: Brian Wo <45139213+brainwo@users.noreply.github.com>

--- lib/src/exception.dart
+++ lib/src/exception.dart
@@ -1,2 +1,6 @@
+// Copyright 2024 Brian Wo. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in LICENSE or at https://opensource.org/license/BSD-3-clause.
+
 /// Exception thrown when parsed file is not a Kara file.
 class NotKaraException implements Exception {}
